a64cd87fa7af10a0f7584da55ca5b496c418284a
Tom Fuertes <tomfuertes@gmail.com>
2013-06-27 17:40:27 -0500

    echo prints colors

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,17 @@
 # Functions
 #-----------------------------------------------------------------------------
 
-# Notice
-function notice { echo  "### Notice: $1 ###"; }
+# Notice title
+notice() { echo -e "\033[1;32m=> $1\033[0m"; }
 
 # Error title
-function error { echo "### Error :( $1 ###"; }
+error() { echo -e "\033[1;31m=> Error: $1\033[0m"; }
 
 # List item
-function c_list { echo  "  ✔ $1"; }
+c_list() { echo -e "  \033[1;32m✔\033[0m $1"; }
 
 # Error list item
-function e_list { echo  "  ✖ $1"; }
+e_list() { echo -e "  \033[1;31m✖\033[0m $1"; }
 
 # Check for dependency
 dep() {
