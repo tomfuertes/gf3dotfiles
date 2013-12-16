28195390516fca3caea99ee897805239900faf1c
Tom Fuertes <tomfuertes@gmail.com>
2013-12-16 14:32:14 -0600

    gifify updates to rm cloudapp dep

--- .scripts/gifify.sh
+++ .scripts/gifify.sh
@@ -7,15 +7,12 @@ function printHelpAndExit {
   echo 'Options: (all optional)'
   echo '  c CROP:   The x and y crops, from the top left of the image, i.e. 640:480'
   echo '  o OUTPUT: The basename of the file to be output (default "output")'
-  echo '  n:        Do not upload the resulting image to CloudApp'
   echo '  x:        Remove the original file and resulting .gif once the script is complete'
   echo ''
   echo 'Example:'
   echo '  gifify -c 240:80 -o my-gif -x my-movie.mov'
   exit $1
 }
-
-noupload=0
 
 OPTERR=0
 
@@ -24,7 +21,6 @@ while getopts "c:o:nx" opt; do
     c) crop=$OPTARG;;
     h) printHelpAndExit 0;;
     o) output=$OPTARG;;
-    n) noupload=1;;
     x) cleanup=1;;
     *) printHelpAndExit 1;;
   esac
@@ -48,13 +44,9 @@ fi
 
 ffmpeg -i $filename $crop -r 10 -f image2pipe -vcodec ppm - | convert -verbose +dither -layers Optimize - ${output}.gif
 
-if [ $noupload -ne 1 ]; then
-  echo `cloudapp -d ${output}.gif`
-
-  if [ $cleanup ]; then
-    rm $filename
-    rm ${output}.gif
-  fi
-else
-  echo ${output}.gif
+echo ${output}.gif
+if [ $cleanup ]; then
+  rm $filename
+  rm ${output}.gif
 fi
+
